7d2146ffa603d31a255a8f6088ed43ff69372dff
veasion <veasion@qq.com>
2024-07-19 16:29:31 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import AiPpt from './components/AiPpt.tsx'
+
+function App() {
+  return (
+    <>
+      <AiPpt />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AiPpt.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function AiPpt() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        AiPPT
+      </div>
+    </>
+  )
+}
+
+export default AiPpt
